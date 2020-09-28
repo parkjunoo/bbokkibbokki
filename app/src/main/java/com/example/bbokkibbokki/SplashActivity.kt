429d8507a6289626d45289a7c071306556c35b86
junsoo <01junsoo@gmail.com>
2020-09-28 20:56:35 +0900

    splash & shake 적용

---
+++ app/src/main/java/com/example/bbokkibbokki/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bbokkibbokki
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 1000 //
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //main화면 불러오기
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
